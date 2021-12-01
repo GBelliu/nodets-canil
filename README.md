0377a81762e85d50c5905ebd5fc21e16f059bf0c
Guilherme Bellissimo Barbosa <gui.bellissimo@gmail.com>
2021-11-30 21:11:33 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # nodets-canil
+
+### Pré-requisitos globais:
+
+'npm i -g nodemon typescript ts-node'
+
+### Instalação
+
+'npm install'
+
+### Rodar o projeto
+
+'npm run start-dev'
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/search', SearchController.search);
+export default router;
